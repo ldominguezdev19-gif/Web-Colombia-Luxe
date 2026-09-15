d5e176ba52d4c83edb8edded4c2e7cce24ab7a68
ldominguezdev19 <ldominguez.dev19@gmail.com>
2026-09-15 15:37:59 -0500

    termino de darle estilo al nav

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ prevBtn.addEventListener('click', function(){
     updateSlide();
 });
 */
+
+/*
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        nav.classList.add('nav-scrolled');
+    } else {
+        nav.classList.remove('nav-scrolled');
+    }
+});
+
+*/
